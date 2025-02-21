b25df667f3ec3492994227abe453294436a61177
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-02-21 12:12:35 -0300

    criando os componentes e o hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
+import UserForm from './components/UserForm';
+import ReviewForm from './components/ReviewForm';
+import Thanks from './components/Thanks';
 import './App.css';
+import { useForm } from './hooks/useForm';
 
 function App() {
+
+  const formComponents = [ <UserForm />, <ReviewForm />, <Thanks /> ];
+  const { currentStep, currentComponent } = useForm(formComponents);
 
   return (
     <div className="app">
@@ -13,7 +20,7 @@ function App() {
         <p></p>
         <form>
           <div className="inputs-container">
-            
+            {currentComponent}
           </div>
           <div className="actions">
             <button type="button">
